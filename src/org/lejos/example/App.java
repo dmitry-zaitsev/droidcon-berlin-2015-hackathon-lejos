3512ea2f36872fb69a9c5c3d59bd4dfb089af451
Vojta <planarvoid@gmail.com>
2015-06-06 15:41:30 +0200

    App

--- src/org/lejos/example/App.java
+++ src/org/lejos/example/App.java
@@ -4,6 +4,7 @@ import lejos.nxt.SensorPort;
 import lejos.nxt.UltrasonicSensor;
 import lejos.robotics.RangeReading;
 import lejos.robotics.objectdetection.*;
+import lejos.nxt.Motor;
 
 /**
  * Example leJOS Project with an ant build file
@@ -21,15 +22,22 @@ public class App {
                 ultrasonicDetectorForPort(SensorPort.S4),
                 DISTANCE_BETWEEN_SENSORS_CM
         );
+        final HackPilot p = new HackPilot(5f, Motor.A, Motor.B, 41, -150, 150);
 
         while (System.currentTimeMillis() - startTime < interval) {
             Feature feature = detector.scan();
             if (feature == null) {
+            	p.travel(10);
                 continue;
             }
 
             RangeReading reading = feature.getRangeReading();
             System.out.println(reading.getAngle() + " " + reading.getRange());
+            if (reading.getAngle() > 0) {
+            	p.travelArc(600, 10);
+            } else {
+            	p.travelArc(-600, 10);            	
+            }
         }
     }
 
